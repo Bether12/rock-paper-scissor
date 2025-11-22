4f980084545e35c1e6c7e48adc4a74eee1a42ed7
Bether12 <elnenogomez13@gmail.com>
2025-11-22 08:37:59 -0500

    Create playRound function with humanChoice and computerChoice as parameters

--- script.js
+++ script.js
@@ -12,5 +12,23 @@ function getHumanChoice(){
     return choice
 }
 
+function playRound(humanChoice, computerChoice){
+    if(humanChoice==computerChoice){
+        return 'Draw!'
+    }else if(humanChoice=='rock'&&computerChoice=='scissor'){
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    }else if(humanChoice=='paper'&&computerChoice=='rock'){
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    }else if(humanChoice=='scissor'&&computerChoice=='paper'){
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    }else{
+        computerScore++;
+        return `You lose! ${humanChoice} beats ${computerChoice}`;
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
