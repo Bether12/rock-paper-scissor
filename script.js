c444c0607a6fbd4e36ff33ab6d8e502c83f8c9dc
Bether12 <elnenogomez13@gmail.com>
2025-11-25 09:27:36 -0500

    Change playRound() output to show in the browser, and add an if/else block to display the winner when reached 5 points and then reset the score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const choice = Math.floor(Math.random()*3);
     if (choice == 0) 
@@ -10,28 +13,82 @@ function getComputerChoice(){
 const results = document.querySelector('.results');
 const human_score = document.querySelector('#human-score');
 const computer_score = document.querySelector('#computer-score');
+const text = document.createElement('p');
+results.appendChild(text);
 
 function playRound(humanChoice, computerChoice){
-    console.log(humanChoice+' '+computerChoice)
     if(humanChoice==computerChoice){
-        console.log('Draw!');
+        human_score.textContent = `Your score: ${humanScore}`;
+        computer_score.textContent = `Computer's score: ${computerScore}`;
+        text.textContent = 'Draw!';
     }else if(humanChoice=='rock'&&computerChoice=='scissor'){
         humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        human_score.textContent = `Your score: ${humanScore}`;
+        computer_score.textContent = `Computer's score: ${computerScore}`;
+        text.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+
+        if(humanScore==5){
+            alert('You win the game!');
+            humanScore = 0;
+            computerScore = 0;
+        }else if(computerScore==5){
+            alert('You lose the game!');
+            humanScore = 0;
+            computerScore = 0;
+        }
+
     }else if(humanChoice=='paper'&&computerChoice=='rock'){
         humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        human_score.textContent = `Your score: ${humanScore}`;
+        computer_score.textContent = `Computer's score: ${computerScore}`;
+        text.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+
+        if(humanScore==5){
+            alert('You win the game!');
+            humanScore = 0;
+            computerScore = 0;
+        }else if(computerScore==5){
+            alert('You lose the game!');
+            humanScore = 0;
+            computerScore = 0;
+        }
+
     }else if(humanChoice=='scissor'&&computerChoice=='paper'){
         humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        human_score.textContent = `Your score: ${humanScore}`;
+        computer_score.textContent = `Computer's score: ${computerScore}`;
+        text.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+
+        if(humanScore==5){
+            alert('You win the game!');
+            humanScore = 0;
+            computerScore = 0;
+        }else if(computerScore==5){
+            alert('You lose the game!');
+            humanScore = 0;
+            computerScore = 0;
+        }
+
     }else{
         computerScore++;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        human_score.textContent = `Your score: ${humanScore}`;
+        computer_score.textContent = `Computer's score: ${computerScore}`;
+        text.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+
+        if(humanScore==5){
+            alert('You win the game!');
+            humanScore = 0;
+            computerScore = 0;
+        }else if(computerScore==5){
+            alert('You lose the game!');
+            humanScore = 0;
+            computerScore = 0;
+        }
+
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+
 
 const buttons = document.querySelector('.buttons');
 buttons.addEventListener('click', (e)=>{
